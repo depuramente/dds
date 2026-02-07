7e042825269a2c5bb5394b5548c92cd7ae1a8dd5
Alex <alexgarciabell@gmail.com>
2026-02-06 18:37:51 -0600

    DPM-001|User Story|Refactoring Customer Service

--- common/src/main/java/com/depuramente/dds/common/constants/setmore/SetmoreConstants.java
+++ common/src/main/java/com/depuramente/dds/common/constants/setmore/SetmoreConstants.java
@@ -1,4 +1,31 @@
 package com.depuramente.dds.common.constants.setmore;
 
 public class SetmoreConstants {
+    private SetmoreConstants() {
+    }
+
+    public static final String BEARER = "Bearer ";
+    public static final String CURSOR = "cursor";
+    public static final String SETMORE_TOKEN_PATH = "/api/v1/o/oauth2/token";
+    public static final String REFRESH_TOKEN = "refreshToken";
+
+    public static final String SETMORE_API_PATH = "/api/v1/bookingapi";
+    public static final String SETMORE_STAFF_PATH = SETMORE_API_PATH + "/staffs";
+
+    // CUSTOMER
+    public static final String SETMORE_CUSTOMER = SETMORE_API_PATH + "/customer";
+    public static final String SETMORE_CREATE_CUSTOMER = SETMORE_CUSTOMER + "/create";
+    public static final String EMAIL = "email";
+    public static final String FIRSTNAME ="firstname";
+    public static final String PHONE = "phone";
+
+    // APPOINTMENT
+    public static final String SETMORE_APPOINTMENT = SETMORE_API_PATH + "/appointment";
+    public static final String SETMORE_CREATE_APPOINTMENT = SETMORE_APPOINTMENT + "/create";
+    public static final String SETMORE_UPDATE_APPOINTMENT_LABEL = SETMORE_APPOINTMENT + "/create";
+
+
+
+//    "/api/v1/bookingapi/customer/create"
+
 }
--- setmore-service/src/main/java/com/depuramente/dds/setmore/services/SetmoreCustomerService.java
+++ setmore-service/src/main/java/com/depuramente/dds/setmore/services/SetmoreCustomerService.java
@@ -1,6 +1,9 @@
 package com.depuramente.dds.setmore.services;
 
-import com.depuramente.dds.common.dto.setmore.customer.*;
+import com.depuramente.dds.common.dto.setmore.customer.CreateCustomerRequest;
+import com.depuramente.dds.common.dto.setmore.customer.CreateCustomerResponse;
+import com.depuramente.dds.common.dto.setmore.customer.CustomerDto;
+import com.depuramente.dds.common.dto.setmore.customer.GetCustomerResponse;
 import jakarta.annotation.Nullable;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
@@ -18,7 +21,7 @@ public class SetmoreCustomerService {
     }
 
     public Mono<CustomerDto> createCustomer(CreateCustomerRequest request) {
-        return client.post(SETMORE_CREATE_CUSTOMER_PATH,
+        return client.post(SETMORE_CREATE_CUSTOMER,
                         request,
                         CreateCustomerResponse.class)
                 .map(res -> res.data().customer());
@@ -27,7 +30,7 @@ public class SetmoreCustomerService {
     public Mono<List<CustomerDto>> getCustomers(String firstName,
                                                 @Nullable String email,
                                                 @Nullable String phone) {
-        return client.get(SETMORE_CUSTOMER_PATH,
+        return client.get(SETMORE_CUSTOMER,
                         GetCustomerResponse.class,
                         uri -> {
 
