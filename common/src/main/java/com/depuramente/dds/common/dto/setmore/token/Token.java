2d7c8433f308777191f94b8c4addc15c55259945
Alex <alexgarciabell@gmail.com>
2026-02-14 23:07:54 -0600

    DPM-001|UserStory|WIP

--- common/src/main/java/com/depuramente/dds/common/dto/setmore/token/Token.java
+++ common/src/main/java/com/depuramente/dds/common/dto/setmore/token/Token.java
@@ -2,9 +2,28 @@ package com.depuramente.dds.common.dto.setmore.token;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record Token(@JsonProperty("access_token") String accessToken,
-                    @JsonProperty("token_type") String tokenType,
-                    @JsonProperty("expires_in") long expiresIn,
-                    @JsonProperty("user_id") String userId
+import java.time.Instant;
+import java.util.List;
+
+public record Token(
+        @JsonProperty("access_token") String accessToken,
+        @JsonProperty("type") String type,
+        @JsonProperty("token_type") String tokenType,
+        @JsonProperty("expires") long expires,
+        @JsonProperty("issued_to") String issuedTo,
+        @JsonProperty("scopes") List<String> scopes,
+        @JsonProperty("access_type") String accessType,
+        @JsonProperty("user_id") String userId,
+        @JsonProperty("project_id") String projectId,
+        @JsonProperty("ok") boolean ok,
+        @JsonProperty("expires_in") long expiresIn
 ) {
+    public boolean isExpired() {
+        return expiresAt().isBefore(Instant.now());
+    }
+
+    public Instant expiresAt() {
+        return Instant.ofEpochMilli(expires);
+    }
 }
+
